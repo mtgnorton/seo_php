a47b1daa0f8f871ea660f1509f05ac751ddaf2eb
zealerChina <zhangfuyinging@163.com>
2021-07-07 16:39:15 +0800

    同上

--- database/migrations/2021_05_25_205541_create_templates_table.php
+++ database/migrations/2021_05_25_205541_create_templates_table.php
@@ -15,8 +15,8 @@ class CreateTemplatesTable extends Migration
     {
         Schema::create('templates', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name', 100)->unique()->default('')->comment('模板名称');
-            $table->string('tag', 100)->unique()->default('')->comment('模板标识');
+            $table->string('name', 100)->default('')->comment('模板名称');
+            $table->string('tag', 100)->default('')->comment('模板标识');
             $table->integer('category_id')->index()->default(0)->comment('分类ID');
             $table->integer('type_id')->index()->default(0)->comment('类型ID');
             $table->string('type_tag', 50)->index()->default('')->comment('类型标签');
