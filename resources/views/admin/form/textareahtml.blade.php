4bd8ec89a8d77363acd86b7748b9fcf715ac2c23
zealerChina <zhangfuyinging@163.com>
2021-07-07 17:15:42 +0800

    模板文件編輯

---
+++ resources/views/admin/form/textareahtml.blade.php
@@ -0,0 +1,18 @@
+<div class="{{$viewClass['form-group']}} {!! !$errors->has($errorKey) ? '' : 'has-error' !!}">
+
+    <label for="{{$id}}" class="{{$viewClass['label']}} control-label">{{$label}}</label>
+
+    <div class="{{$viewClass['field']}}">
+
+        @include('admin::form.error')
+
+        <textarea name="{{$name}}" class="form-control {{$class}}" rows="{{ $rows }}" placeholder="{{ $placeholder }}" {!! $attributes !!} ><?php echo $value;?></textarea>
+
+        {!! $append !!}
+
+        
+        @include('admin::form.help-block')
+
+    </div>
+</div>
+
