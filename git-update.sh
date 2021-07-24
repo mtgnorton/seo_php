c58bcd6b70f051beb61ddf23e566263b22ad0c74
yaohong.lin <admin@example.com>
2021-07-24 17:44:47 +0800

    auto commit

--- git-update.sh
+++ git-update.sh
@@ -1,13 +1,21 @@
 cd /home/seo_master
+echo "切换到seo_master目录<br>";
+
 sudo  git reset --hard
 sudo git pull
+echo "<br>";
 rsync -avz /home/seo_master/* /home/seo_github/ --exclude='.git'
+echo "<br>";
+
 php encode_files_linux.php
+echo "<br>";
 
 cd /home/seo_github
+echo "切换到seo_github目录<br>";
 
 git add .
 
 git commit -am "auto commit"
 
 git push
+echo "提交完成"
