a7cddbb92bfc7257e16a65259655f7672f0d9915
yaohong.lin <admin@example.com>
2021-07-24 17:38:51 +0800

    auto commit

--- git-update.sh
+++ git-update.sh
@@ -2,3 +2,11 @@ sudo  git reset --hard
 sudo git pull
 rsync -avz /home/seo_master/* /home/seo_github/ --exclude='.git'
 php encode_files_linux.php
+
+cd /home/seo_github
+
+git add .
+
+git commit -am "auto commit"
+
+git push
