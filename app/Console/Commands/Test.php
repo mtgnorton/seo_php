c96a0dac7a65ded4e2f243e58b1bc2120b4e6d9f
yaohong.lin <admin@example.com>
2021-07-28 17:17:57 +0800

    auto commit

--- app/Console/Commands/Test.php
+++ app/Console/Commands/Test.php
@@ -49,7 +49,11 @@ class Test extends Command
      */
     public function handle()
     {
-        dump(FakerOriginService::toSynonym());
+        $data = '[北京航空客运业务7月26日全面转场大兴机场 ]
+<p class="otitle">（原文章标题：北京航空货物运输业务流程7月26日全方位转换场地北京大兴机场）</p><p>7月26日早上，由北京航空执飞的CA8651飞机航班顺利从北京大兴机场起降，意味着北京航空宣布打开在北京大兴机场的运作，执飞北京大兴来回合肥市、揭阳市广东潮汕、绵阳市、沈阳市、重庆市、江阴6条航道12个飞机航班。</p><p>据统计，做为我国国内航空股权有限责任公司的子公司，北京航空借助中国国航安全性体系管理和資源，在进行公务机有关业务流程的与此同时，积极主动拓展航空服务专业范畴，于2018年根据了我国民用航空局CCAR-121部运作达标核准，并于同一年完成了公共性航空货运的首航。</p><p>北京航空以北京市为产业基地，执飞机场型为波音777-800，转换场地至北京大兴机场后将进一步数据加密北京大兴机场航道互联网。现阶段，除国家公务航空公司业务流程仍在首都国际机场运作外，北京航空货物运输业务流程已所有转至北京大兴机场。</p><p>编写/彭小菲</p><p><img src="https://static.ws.126.net/163/f2e/product/post_nodejs/static/logo.png"><img src="https://static.ws.126.net/cnews/css13/img/end_news.png" alt="netease" width="13" height="12" class="icon">文中来源于：北青报责编：姚文体局_NN1682';
+
+
+        dump(FakerOriginService::toLength5118($data));
         exit;
     }
 
