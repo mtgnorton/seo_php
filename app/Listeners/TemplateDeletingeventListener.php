08005ec83560a838a4d1a26f159ccdeeaf69ab55
yaohong.lin <admin@example.com>
2021-07-28 15:15:30 +0800

    auto commit

--- app/Listeners/TemplateDeletingeventListener.php
+++ app/Listeners/TemplateDeletingeventListener.php
@@ -2,12 +2,14 @@
 
 namespace App\Listeners;
 
+use App\Constants\RedisCacheKeyConstant;
 use App\Events\TemplateDeletingEvent;
 use App\Events\WebsiteTemplateDeletingEvent;
 use App\Models\Template;
 use Exception;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Storage;
 
 class TemplateDeletingeventListener
@@ -45,6 +47,10 @@ class TemplateDeletingeventListener
         foreach ($materials as $material) {
             $material->delete();
         }
+
+        // 删除缓存
+        $key = RedisCacheKeyConstant::CACHE_TEMPLATES;
+        Cache::forget($key);
 
         // // 域名刪除
         // $websiteTemplates = $template->websiteTemplates;
