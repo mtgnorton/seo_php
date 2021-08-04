14fcc7935d13f9e1eb1e3a787d04ba1b071a1235
yaohong.lin <admin@example.com>
2021-08-04 11:38:35 +0800

    auto commit

--- database/migrations/2021_05_19_005608_create_files_table.php
+++ database/migrations/2021_05_19_005608_create_files_table.php
@@ -21,7 +21,7 @@ class CreateFilesTable extends Migration
             $table->bigInteger('size')->nullable()->default(0)->comment('文件大小(字节)');
             $table->integer('rows')->nullable()->default(0)->comment('行数');
             $table->integer('success_rows')->nullable()->default(0)->comment('成功行数');
-            $table->text('message')->comment('上传结果信息');
+            $table->string('message')->nullable()->default('')->comment('上传结果信息');
             $table->enum('type', [
                 'article', 'column', 'diy',
                 'image', 'video', 'sentence',
--- database/migrations/2021_05_29_173926_create_keywords_table.php
+++ database/migrations/2021_05_29_173926_create_keywords_table.php
@@ -19,6 +19,7 @@ class CreateKeywordsTable extends Migration
             $table->string('tag', 20)->index()->default('')->comment('标签');
             $table->integer('category_id')->index()->default(0)->comment('分类ID');
             $table->integer('file_id')->nullable()->default(0)->comment('文件ID');
+            $table->tinyInteger('is_collected')->nullable()->default(0)->comment('是否是采集: 1.是,0.不是');
             $table->timestamps();
         });
         DB::statement("ALTER TABLE `keywords` comment '关键词表'"); // 表注释
