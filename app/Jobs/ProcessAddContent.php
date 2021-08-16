1c5b0c6de5ebda87fac5c1d42d276ddf33d6f224
yaohong.lin <admin@example.com>
2021-08-16 12:19:06 +0800

    auto commit

--- app/Jobs/ProcessAddContent.php
+++ app/Jobs/ProcessAddContent.php
@@ -41,8 +41,7 @@ class ProcessAddContent implements ShouldQueue
         ContentService::insertContentByFile(
             $data['categoryId'],
             $data['type'],
-            $data['fileObj'],
-            $data['fileData']
+            $data['fileObj']
         ); 
     }
 
