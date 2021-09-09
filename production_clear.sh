9d543cf433147aeeaa11db8aa4cbaf0a6a186206
yaohong.lin <admin@example.com>
2021-09-09 17:37:00 +0800

    auto commit

--- production_clear.sh
+++ production_clear.sh
@@ -1,4 +1,4 @@
-/usr/local/bin/php artisan config:clear
-/usr/local/bin/php artisan route:clear
-/usr/local/bin/php artisan clear-compiled
-/usr/local/bin/php artisan admin:minify --clear
+/usr/local/bin/php /data/wwwroot/seo_php/artisan config:clear
+/usr/local/bin/php /data/wwwroot/seo_php/artisan route:clear
+/usr/local/bin/php /data/wwwroot/seo_php/artisan clear-compiled
+/usr/local/bin/php /data/wwwroot/seo_php/artisan admin:minify --clear
