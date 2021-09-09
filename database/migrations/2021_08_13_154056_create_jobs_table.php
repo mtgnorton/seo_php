abec4cc66c808b0dc429f975678ed23764bb9182
yaohong.lin <admin@example.com>
2021-09-09 16:04:50 +0800

    auto commit

--- database/migrations/2021_08_13_154056_create_jobs_table.php
+++ database/migrations/2021_08_13_154056_create_jobs_table.php
@@ -17,7 +17,7 @@ class CreateJobsTable extends Migration
             $table->bigIncrements('id');
             $table->string('queue')->index();
             $table->longText('payload');
-            $table->unsignedTinyInteger('attempts');
+            $table->unsignedInteger('attempts');
             $table->unsignedInteger('reserved_at')->nullable();
             $table->unsignedInteger('available_at');
             $table->unsignedInteger('created_at');
