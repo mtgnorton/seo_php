75fbb130ec07eb5c8009ba2c20588f9c9a10d6f7
yaohong.lin <admin@example.com>
2022-01-22 21:22:10 +0800

    auto commit

---
+++ app/Jobs/ProcessClearCache.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Services\CommonService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ProcessClearCache implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $data;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        common_log('开始进行缓存的全面清除');
+        CommonService::clearCacheAll($this->data);
+        common_log('缓存的全面清除完成');
+    }
+}
