217116b71b07675aa630687f8f48ccb6ef7dc0ab
yaohong.lin <admin@example.com>
2022-08-05 10:01:37 +0800

    auto commit

--- update/modify.sql
+++ update/modify.sql
@@ -1,5 +1,4 @@
 # 系统更新sql demo,每个sql语句以;结尾换行分隔
 
-
-
-alter table gathers add no_regular_url text default null comment "忽略的网址正则,一行一条" after regular_url;
+ALTER TABLE `seo_php`.`gathers` 
+ADD COLUMN `no_regular_url` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL COMMENT '忽略的网址正则,一行一条';
